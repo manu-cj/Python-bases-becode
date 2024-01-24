e0f09d813a32ad2c3f97bdc7d17ecce13ba0908a
manucarenjot <92257106+manucarenjot@users.noreply.github.com>
2024-01-24 11:48:45 +0000

    Fin de l'exo Built-in functions

--- exo_3.py
+++ exo_3.py
@@ -7,13 +7,30 @@ pi = 3.14
 pi = round(pi, 2)
 
 reversed_text = "Hello world !"
-rever = ''.join(reversed(reversed_text))
+revers = ''.join(reversed(reversed_text))
 
-print(rever)
+print(list(revers))
 
 print(f"What is you age ?")
 age = int(input())
 print(age)
 print(f"you have {age} years (type is {type(age).__name__})")
 
-#Exo6
+num = [2, 8, 1, 4, 6, 3, 7]
+
+sorted_num = sorted(num)
+sum_of_list = sum(num)
+min_value = min(num)
+max_value = max(num)
+calc = "1 + 2"
+
+string_interpreted = eval(calc)
+
+
+print(string_interpreted)
+
+
+
+
+    
+    
